6c13cd898058a4caa23c52902cf261d899697a59
Kylian <kylianmayanga@gmail.com>
2022-03-09 13:51:48 +0100

    setting up the css variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import Service from './components/service/Service'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
       <div>
         <Header/>
+        <Nav/>
+        <About/>
+        <Service/>
+        <Portfolio/>
+        <Experience/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
       </div>
   )
 }
