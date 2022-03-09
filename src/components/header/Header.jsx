e702af4805926a40fe5b98aacee4b0b4b463740f
Kylian <kylianmayanga@gmail.com>
2022-03-09 15:29:54 +0100

    the header is done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import CTA from './CTA'
+import './Header.css'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Kylian Mayanga-Fournier</h1>
+        <h5 className='text-light'>Fullstack Developper</h5>
+        <CTA/>
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt='' />
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
