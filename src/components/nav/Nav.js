8f4b7e03a5918b88d10ed8d94667147c4ea7beb7
Kylian <kylianmayanga@gmail.com>
2022-03-11 11:53:11 +0100

    update the nav bar and added the testimonials

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,6 +5,7 @@ import {FaUserAlt} from 'react-icons/fa'
 import {MdHomeRepairService} from 'react-icons/md'
 import {BsFillBookFill} from 'react-icons/bs'
 import {AiFillMessage} from 'react-icons/ai'
+import {FaComments} from 'react-icons/fa'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -16,7 +17,8 @@ const Nav = () => {
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''} ><FaUserAlt/></a>
       <a href='#experience'onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}><BsFillBookFill/></a>
       <a href='#service'onClick={() => setActiveNav('#service')} className={activeNav === "#service" ? 'active' : ''}><MdHomeRepairService/></a>
-      <a href='#contact'onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><AiFillMessage/></a>
+      <a href='#testimonials'onClick={() => setActiveNav('#testimonials')} className={activeNav === "#testimonials" ? 'active' : ''}><AiFillMessage/></a>
+      <a href='#contact'onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><FaComments/></a>
     </nav>
   )
 }
