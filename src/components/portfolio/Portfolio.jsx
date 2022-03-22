24462e909af5899ce4c162c8f93e8c233afb9823
Kylian <kylianmayanga@gmail.com>
2022-03-22 17:55:44 +0100

    add de windBnb to the portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Portfolio.css'
 import {IoCodeWorkingSharp} from 'react-icons/io5'
+import WindBnb from '../../assets/WindBnb.png'
 
 const data = [
   {
     id: 1,
-    image: '',
-    title: "work is in progress",
+    image: WindBnb,
+    title: "WindBnb",
     github: 'https://github.com',
-    demo: '',
+    demo: 'https://rad-gumdrop-e414d1.netlify.app/',
   },
 ]
 
@@ -22,15 +23,14 @@ const Portfolio = () => {
           data.map(({id, image, title, github, demo}) =>{
             return (
               <article className='portfolio_item'>
-                  {/* <div key={id} className="portfolio_item-image">
+                  <div key={id} className="portfolio_item-image">
                     <img src={image} alt={title} />
-                  </div> */}
-                  <IoCodeWorkingSharp/>
+                  </div>
                   <h3>{title}</h3>
-                  {/* <div className="portfolio_item-cta">
+                  <div className="portfolio_item-cta">
                     <a href={github} rel="noreferrer" className='btn' target='_blank'>Github</a>
                     <a href={demo} rel="noreferrer" className='btn btn-primary' target='_blank'>Live Demo</a>
-                  </div> */}
+                  </div>
               </article>
             )
           })
