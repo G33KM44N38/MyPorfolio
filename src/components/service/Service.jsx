88bd9b44287b763725b2623cc41224ed60a9d68c
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-02-09 14:56:29 +0100

    Add my backend services

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,63 +1,92 @@
 import React from 'react'
 import './Service.css'
-import {AiOutlineCheck} from 'react-icons/ai'
+import { AiOutlineCheck } from 'react-icons/ai'
+
+const FrontEnd = [
+	{
+		id: 1,
+		name: "Static Website",
+	},
+	{
+		id: 2,
+		name: "Dynamic Website",
+	},
+	{
+		id: 3,
+		name: "Mobile App",
+	},
+	{
+		id: 4,
+		name: "Static Website",
+	},
+	{
+		id: 5,
+		name: "Web Application",
+	}
+]
+
+const BackEnd = [
+	{
+		id: 1,
+		name: "Web API",
+	},
+	{
+		id: 2,
+		name: "database",
+	},
+]
 
 const Service = () => {
-  return (
-    <section id='service'>
-      <h5>What I offer</h5>
-      <h2>Services</h2>
+	return (
+		<section id='service'>
+			<h5>What I offer</h5>
+			<h2>Services</h2>
 
-      <div className="container services_container">
-        <article className='service'>
-          <div className="service_head">
-            <h3>Frontend</h3>
-          </div>
+			<div className="container services_container">
+				<article className='service'>
 
-          <ul className='service_list'>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Static website</p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Dynamyc website</p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Responsive web page</p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Web Application</p>
-            </li>
-          </ul>
-        </article>
+					<div className="service_head">
+						<h3>Frontend</h3>
+					</div>
 
-        {/* BACKEND */}
-        {/* <article className='service'>
-          <div className="service_head">
-            <h3>Backend</h3>
-          </div>
+					<ul className='service_list'>
+						{
+							FrontEnd.map(({ id, name }) => {
+								return (
 
-          <ul className='service_list'>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Handle different user</p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-              <p>Knowledge of APIs</p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service_list-icon'/>
-               <p>Host your page to AWS services</p>
-            </li>
-          </ul>
-        </article> */}
-      </div>
-    </section>
-  )
+									<li key={id}>
+										<AiOutlineCheck className='service_list-icon' />
+										<p>{name}</p>
+									</li>
+								)
+							})
+						}
+					</ul>
+				</article>
+
+				<article className='service'>
+
+					<div className="service_head">
+						<h3>BackEnd</h3>
+					</div>
+
+					<ul className='service_list'>
+						{
+							BackEnd.map(({ id, name }) => {
+								return (
+
+									<li key={id}>
+										<AiOutlineCheck className='service_list-icon' />
+										<p>{name}</p>
+									</li>
+								)
+							})
+						}
+					</ul>
+				</article>
+			</div>
+		</section>
+	)
 }
- 
+
 export default Service
